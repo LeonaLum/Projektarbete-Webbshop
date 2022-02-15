aa53d99988d0374cc263d18c1c6059a9ae4c3fdd
LeoL <leona.lumpini@hotmail.com>
2022-02-15 15:41:49 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+
+let products = [];
+
+async function getJsonData() {
+  const response = await fetch('./products.json');
+  const productData = await response.json();
+  console.log(productData)
+  getEveryProduct(productData)
+  
+
+}
+getJsonData();
+
+
+function getEveryProduct(arr) {
+  arr.forEach(product => {
+    console.log(product);
+    return product;
+  });
+}

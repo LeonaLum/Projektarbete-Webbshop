14a8d8b851164ca397c53ed32e2a8f9754d425dc
LeoL <leona.lumpini@hotmail.com>
2022-02-15 16:25:20 +0100

    updated

--- script.js
+++ script.js
@@ -1,28 +1,23 @@
-// test
+
+// För att nå produkterna så behöver du gå in på products i json objektet.
 
 
-async function getAllProducts() {
-  const response = await fetch('./products.json');
-  const productData = await response.json();
-<<<<<<< HEAD
-  console.log(productData)
-  const productsArray = [...productData.products];
-  console.log(productsArray)
-    return productsArray;
+// async function getAllProducts() {
+//   const response = await fetch('./products.json');
+//   const productData = await response.json();
+//   const productsArray = [...productData.products];
+//   console.log(productsArray)
+//     return productsArray;
   
-}
-getAllProducts();
+// }
+// getAllProducts();
 
-=======
-  console.log(productData);
-  getEveryProduct(productData);
-}
-getJsonData();
 
-function getEveryProduct(arr) {
-  arr.forEach((product) => {
-    console.log(product);
-    return product;
-  });
-}
->>>>>>> d43ca3d2ce0b68ab6dcba22c7f306840446dde96
+
+
+
+
+
+
+
+

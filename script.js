176a3b7a2a44db3c2cfa022e34e483040609cb8a
LeoL <leona.lumpini@hotmail.com>
2022-02-16 21:39:19 +0100

    Added detail view for products, added purchase button

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 // För att nå produkterna så behöver du gå in på products i json objektet.
 
+
+
 let productsList = document.getElementById("productsList");
+let detailContainer = document.getElementById("detailContainer");
+
+let shoppingCart = [];
+
+
+
 
 const params = new URLSearchParams(location.search);
 let currentCategory = params.get("category");
+let currentId = params.get("id");
+
 
 switch(currentCategory){
   case"phones":
@@ -19,6 +29,19 @@ switch(currentCategory){
     getAllProducts();
   break;
 }
+
+
+if(currentId){
+  getProductsForDetails();
+}
+
+async function getProductsForDetails() {
+  const response = await fetch('./products.json');
+  const productData = await response.json();
+  const productsArray = [...productData.products];
+    showCardDetails(productsArray);
+}
+
 
 
 
@@ -74,12 +97,58 @@ function createProductCard(arr){
   arr.forEach((product) => {
     let productCard = document.createElement("div");
     productCard.innerHTML=`
+    <a id="${product.id}"class="clickableProductCard"
+    href="productDetail.html?id=${product.id}">
     <h2>${product.name}</h2>
-    <div class="priceContainer"><p>${product.price}</p><div>`;
-
+    <div class="priceContainer"><p>${product.price}</p><div>
+    </a>`;
     productCard.classList.add("productCard");
-    productsList.appendChild(productCard)
-  })
-
-
+    productsList.appendChild(productCard);
+ })
+ 
 }
+
+
+
+
+function showCardDetails(arr){
+  arr.filter((product) => {
+   if(product.id == currentId){
+     let detailCard = document.createElement("div");
+     detailCard.innerHTML = `
+   
+     <article class="detail-Card">
+     <div class="product-Detail-Picture"></div>
+
+      <div class="product-Detail-Desc">
+       <h2>${product.name}</h2> 
+       <p>Product description</p>
+       <p>Pris: </p>
+       <div class="price-Container-Detail">
+       <p>${product.price}</p>
+       </div>
+       <button id="buttonPurchase" class="button-Purchase">Köp</button>
+       </div>
+
+     
+   
+      
+
+     </article>
+
+
+ 
+
+ 
+
+     `;
+     detailContainer.appendChild(detailCard);
+
+     const buttonPurchase = document.getElementById("buttonPurchase")
+     buttonPurchase.addEventListener("click", () => {
+       shoppingCart.push(product);
+       console.log(shoppingCart)
+     })
+   }
+  })
+ }

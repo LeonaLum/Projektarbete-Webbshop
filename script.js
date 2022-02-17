9b398771785a459250ae9576d700e77d930257bd
LeoL <leona.lumpini@hotmail.com>
2022-02-17 21:39:34 +0100

    Started with shoppingCart, synced products with localStorage

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
 // För att nå produkterna så behöver du gå in på products i json objektet.
 
 //Nyaste scriptet
+let shoppingCartArray = [];
 
 
-let productsListMain = document.getElementById("productsListMain");
-let detailContainer = document.getElementById("detailContainer");
+
+checkLocalStorageForProducts();
+function checkLocalStorageForProducts(){
+
+  if(localStorage.getItem("0") !== null){
+   let savedProducts = Object.values(localStorage);
+    savedProducts.forEach((product) => {
+    shoppingCartArray.push(JSON.parse(product));
+    console.log(shoppingCartArray)
+  })
+ }
+}
+
+
+
+const productsListMain = document.getElementById("productsListMain");
+const detailContainer = document.getElementById("detailContainer");
 let listTitle = document.getElementById("listTitle");
+let productNumber = localStorage.length;
+console.log(productNumber)
 
-let shoppingCart = [];
+let shoppingCartMain = document.getElementById("shoppingCartMain");
 
 
 
@@ -130,8 +148,6 @@ function createProductCard(arr){
 }
 
 
-
-
 function showCardDetails(arr){
   arr.filter((product) => {
    if(product.id == currentId){
@@ -163,20 +179,41 @@ function showCardDetails(arr){
 
        </div>
      </article>
-
-
- 
-
- 
-
      `;
+
      detailContainer.appendChild(detailCard);
 
      const buttonPurchase = document.getElementById("buttonPurchase")
+     
      buttonPurchase.addEventListener("click", () => {
-       shoppingCart.push(product);
-       console.log(shoppingCart)
+       localStorage.setItem(JSON.stringify(productNumber), JSON.stringify(product));
+       shoppingCartArray.push(product);
+        productNumber++;
+       console.log(shoppingCartArray)
      })
    }
   })
  }
+
+
+ if(location.pathname == "/shoppingCart.html"){
+   createShoppingCartList(shoppingCartArray);
+ }
+
+ function createShoppingCartList(arr){
+  arr.forEach((product) => {
+    let shoppingCartProductCard = document.createElement("div");
+    shoppingCartProductCard.classList.add("product-Card-ShoppingCart");
+    shoppingCartProductCard.innerHTML = `
+    <h2>${product.name}</h2>
+    <div class="product-ShoppingCart-Picture"></div>
+    <div>${product.price}</div>
+    <button class="plus-Minus-Buttons">+</button>
+    <button class="plus-Minus-Buttons">-</button>`
+    shoppingCartMain.appendChild(shoppingCartProductCard)
+    
+  })
+ }
+
+
+

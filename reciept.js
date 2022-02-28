b2f549f5b65c639e0f4e2cfa4ea62f8eca85e2cc
Kalle Sandholm <kallesandholm@gmail.com>
2022-02-28 15:01:00 +0100

    changed some details in json and working on design for recipe-page

--- reciept.js
+++ reciept.js
@@ -1,30 +1,31 @@
-if (location.search == "?location=reciept") {
-    const getUser = JSON.parse(window.localStorage.getItem('user')); //parsar json data från localstorage, hämtar & lägger dessa i en variabel
-    console.log(getUser);
-    
-    let hideOrderform = document.querySelector(".form-main"); //display none 
-    hideOrderform.classList.add("hide");                      // --
-    
-    
-    let targetHeader = document.querySelector(".header"); 
-    const recieptWrapper = document.createElement('div'); 
-    const recieptDiv = document.createElement('div');
-    const recieptSecondDiv = document.createElement('div');
-    console.log(recieptDiv);
-    
-    recieptWrapper.classList.add("reciept-wrapper");
-    recieptDiv.classList.add("reciept-div");
-    recieptSecondDiv.classList.add("reciept-second-div");
-    
-    targetHeader.appendChild(recieptWrapper); 
-    recieptWrapper.appendChild(recieptDiv);
-    recieptWrapper.appendChild(recieptSecondDiv);
-    
+if (location.search == '?location=reciept') {
+  const getUser = JSON.parse(window.localStorage.getItem('user')); //parsar json data från localstorage, hämtar & lägger dessa i en variabel
+  console.log(getUser);
 
+  let hideOrderform = document.querySelector('.form-main'); //display none
+  hideOrderform.classList.add('hide'); // --
 
-    
-    recieptDiv.innerHTML = `
-    <h2> Stort TACK för din order, ${getUser.firstName}! </h2>
+  let targetHeader = document.querySelector('.header');
+  const recieptWrapper = document.createElement('div');
+  const recieptDiv = document.createElement('div');
+  const recieptSecondDiv = document.createElement('div');
+  const recieptMainHeader = document.createElement('div');
+  console.log(recieptDiv);
+
+  recieptWrapper.classList.add('reciept-wrapper');
+  recieptDiv.classList.add('reciept-div');
+  recieptSecondDiv.classList.add('reciept-second-div');
+  recieptMainHeader.classList.add('shopping-Cart-Head');
+
+  targetHeader.appendChild(recieptMainHeader);
+
+  targetHeader.appendChild(recieptWrapper);
+  recieptWrapper.appendChild(recieptDiv);
+  recieptWrapper.appendChild(recieptSecondDiv);
+
+  recieptMainHeader.innerHTML = `<h2> Tack för din order, ${getUser.firstName}! </h2>`;
+
+  recieptDiv.innerHTML = `
     <h3> Ordern skickas inom 1-2 arbetsdagar. </h3>
     <p> Namn: ${getUser.firstName} </p>
     <p> Efternamn: ${getUser.lastName} </p>
@@ -38,20 +39,19 @@ if (location.search == "?location=reciept") {
     <h3> Total summa: ${totalPriceOfProducts}:-</p>
     `;
 
-    recieptSecondDiv.innerHTML =`
+  recieptSecondDiv.innerHTML = `
     <h5> Du har beställt: <h5>
     
     `;
-function showProductsFromCart() {
+  function showProductsFromCart() {
     shoppingCartArray.forEach((product) => {
-        recieptSecondDiv.innerHTML += `
+      recieptSecondDiv.innerHTML += `
         <h4>${product.name}</h2>
         <p>x${product.amount}</p>
         <p>${product.price} kr</p>
         </br>`;
-        });
-    }
-    
-    showProductsFromCart();
-    
-};
+    });
+  }
+
+  showProductsFromCart();
+}

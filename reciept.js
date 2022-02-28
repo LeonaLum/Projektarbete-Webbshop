cf5f02033eac95ffabf544b2ba8e43d8e3697a9d
Kalle Sandholm <kallesandholm@gmail.com>
2022-02-28 17:33:05 +0100

    added better styling for reciept

--- reciept.js
+++ reciept.js
@@ -16,18 +16,27 @@ if (location.search == '?location=reciept') {
   recieptDiv.classList.add('reciept-div');
   recieptSecondDiv.classList.add('reciept-second-div');
   recieptMainHeader.classList.add('shopping-Cart-Head');
+  recieptMainHeader.style.backgroundColor = 'white';
+  recieptMainHeader.style.color = 'rgb(27, 99, 255)';
+  recieptMainHeader.style.display = 'flex';
+  recieptMainHeader.style.flexDirection = 'column';
+  recieptMainHeader.style.marginBottom = '20px';
 
+  recieptMainHeader.style.textAlign = 'center';
   targetHeader.appendChild(recieptMainHeader);
 
   targetHeader.appendChild(recieptWrapper);
   recieptWrapper.appendChild(recieptDiv);
   recieptWrapper.appendChild(recieptSecondDiv);
 
-  recieptMainHeader.innerHTML = `<h2> Tack för din order, ${getUser.firstName}! </h2>`;
+  recieptMainHeader.innerHTML = `
+  <i class="fa-solid fa-truck fa-2x"></i>
+  <h2> Tack för din order, ${getUser.firstName}! </h2>
+  `;
 
   recieptDiv.innerHTML = `
-    <h3> Ordern skickas inom 1-2 arbetsdagar. </h3>
-    <p> Namn: ${getUser.firstName} </p>
+    <h3 class="reciept-deliverytext">Din order skickas inom 1-2 arbetsdagar.</h3>
+    <p class="first-p"> Namn: ${getUser.firstName} </p>
     <p> Efternamn: ${getUser.lastName} </p>
     <p> Adress: ${getUser.adress} </p>
     <p> Postnummer: ${getUser.postalCode} </p>
@@ -40,18 +49,16 @@ if (location.search == '?location=reciept') {
     `;
 
   recieptSecondDiv.innerHTML = `
-    <h5> Du har beställt: <h5>
+    <h3> Du har beställt: </h3>
     
     `;
   function showProductsFromCart() {
     shoppingCartArray.forEach((product) => {
       recieptSecondDiv.innerHTML += `
-        <h4>${product.name}</h2>
+        <p>${product.name}</p>
         <p>x${product.amount}</p>
-        <p>${product.price} kr</p>
-        </br>`;
+        <p class="recipe-bottom-product">${product.price} kr</p>`;
     });
   }
-
   showProductsFromCart();
 }

8a805c4fca59d3e697e2ef8494ad37d43cff41db
LeoL <leona.lumpini@hotmail.com>
2022-03-01 10:31:45 +0100

    accepted changes

--- changeheader.js
+++ changeheader.js
@@ -31,16 +31,10 @@ if (location.search == '?category=all') {
 }
 
 if (location.search == '?category=phones') {
-<<<<<<< HEAD
-  heroHeader.style.backgroundImage = "linear-gradient(rgba(37, 37, 37, 0.767), rgba(255, 254, 254, 0)), url('./img/header/phone-header.png')";
-  heroTitle.innerText = 'Iphone 13 Pro Erbjudande!';
-  heroText.innerText = 'Ny mobil? köp Iphone 13 Pro hos oss och få mobilabonnemang + spotify i ett år (värde 1999kr) på köpet!';
-=======
   heroHeader.style.backgroundImage =
     "linear-gradient(rgba(37, 37, 37, 0.767), rgba(255, 254, 254, 0)), url('./img/header/iphone13pro.jpg')";
   heroTitle.innerText = 'Marknadens främsta mobiltelefon';
   heroText.innerText = 'Köp iPhone 13 Pro hos oss och få AirPods Pro på köpet';
->>>>>>> 56c7c00e7d23cf0b66dee2b60b4e3d41807bf0df
 }
 
 if (location.search == '?category=computers') {
@@ -60,19 +54,11 @@ if (location.search == '?category=consoles') {
 }
 
 if (location.search == '?category=game') {
-<<<<<<< HEAD
-  heroHeader.style.backgroundImage = "linear-gradient(rgba(37, 37, 37, 0.767), rgba(255, 254, 254, 0)), url('./img/header/spelheader.png')";
-  heroTitle.innerText = 'Playstation 5';
-  heroText.innerText = 'Vi har Playstation 5 redo att skickas hem till dig!';
-
-
-=======
   heroHeader.style.backgroundImage =
     "linear-gradient(rgba(37, 37, 37, 0.767), rgba(255, 254, 254, 0)), url('./img/header/spelheader.jpg')";
   heroTitle.innerText = 'Halo Infinite: Ett rent mästerverk';
   heroText.innerText =
     '20 år efter att Master Chief såg dagsljus, så är det dags för en ny legendarisk campaign';
->>>>>>> 56c7c00e7d23cf0b66dee2b60b4e3d41807bf0df
 }
 
 //För att se hur många produkter som finns i shopping cart
